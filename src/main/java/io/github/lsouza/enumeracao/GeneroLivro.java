c0b5aad15e2155bb7f2c4e465a45534486972368
Leonardo <leonardo_vieira199@hotmail.com>
2026-02-02 11:08:40 -0300

    mapeamento jpa - models

---
+++ src/main/java/io/github/lsouza/enumeracao/GeneroLivro.java
@@ -0,0 +1,10 @@
+package io.github.lsouza.enumeracao;
+
+public enum GeneroLivro {
+    FICCAO,
+    FANTASIA,
+    MISTERIO,
+    ROMANCE,
+    BIOGRAFIA,
+    CIENCIA
+}
---
+++ src/main/java/io/github/lsouza/models/Autor.java
@@ -0,0 +1,34 @@
+package io.github.lsouza.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "autor", schema = "public")
+public class Autor {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome", length = 100, nullable = false)
+    private String nome;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "nacionalidade", length = 50, nullable = false)
+    private String nascionalidade;
+
+    @OneToMany(mappedBy = "autor")
+    private List<Livro> livros;
+
+}
