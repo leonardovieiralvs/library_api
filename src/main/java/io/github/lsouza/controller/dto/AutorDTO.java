0b77d46032127d3fb09f69e6916c14850e88c705
Leonardo <leonardo_vieira199@hotmail.com>
2026-02-13 11:54:30 -0300

    dto

--- src/main/java/io/github/lsouza/controller/dto/AutorDTO.java
+++ src/main/java/io/github/lsouza/controller/dto/AutorDTO.java
@@ -3,13 +3,20 @@ package io.github.lsouza.controller.dto;
 import io.github.lsouza.models.Autor;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record AutorDTO(UUID id,
-                       @NotBlank String nome,
-                       @NotNull LocalDate dataNascimento,
-                       @NotBlank String nacionalidade) {
+                       @NotBlank(message = "Campo obrigatório.") @Size(min = 2, max = 50, message = "Campo fora do tamanho padrão.")
+                       String nome,
+
+                       @NotNull(message = "Campo obrigatório.") @Past(message = "Não pode ser uma data futura.")
+                       LocalDate dataNascimento,
+
+                       @NotBlank(message = "Campo obrigatório.") @Size(min = 2, max = 50, message = "Campo fora do tamanho padrão.")
+                       String nacionalidade) {
 
 }
