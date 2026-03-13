67c14b91f0e0a984198bf16f0cefbeaa2c35a82a
Leonardo <leonardo_vieira199@hotmail.com>
2026-03-13 07:01:53 -0300

    formLogin

---
+++ src/main/java/io/github/lsouza/controller/LoginViewController.java
@@ -0,0 +1,13 @@
+package io.github.lsouza.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginViewController {
+
+    @GetMapping("/login")
+    public String paginaLogin() {
+        return "login";
+    }
+}
