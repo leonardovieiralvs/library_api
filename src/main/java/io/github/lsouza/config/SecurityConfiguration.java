39f7d8d1d6c02cc451e194121bea7b6f9f92bc3d
Leonardo <leonardo_vieira199@hotmail.com>
2026-03-13 19:20:11 -0300

    encoded password

--- src/main/java/io/github/lsouza/config/SecurityConfiguration.java
+++ src/main/java/io/github/lsouza/config/SecurityConfiguration.java
@@ -6,6 +6,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +28,29 @@ public class SecurityConfiguration {
                     authorize.anyRequest().authenticated();
                 })
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService (PasswordEncoder encoder) {
+
+        UserDetails user1 = User.builder()
+                .username("lvs")
+                .password(encoder.encode("12345"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user2 = User.builder()
+                .username("bira")
+                .password(encoder.encode("12345"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
